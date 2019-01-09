20be4274b5511355355513c5f90300d8eb4082d4
wohaofang <740148050@qq.com>
2019-01-09 10:55:22 +0800

    feat: init

---
+++ once/hello_world.dart
@@ -0,0 +1,7 @@
+
+
+// Main 程序入口 !!!
+void main(){
+  // 控制台打印
+  print("Hello World!");
+}
---
+++ second/var_and_const.dart
@@ -0,0 +1,16 @@
+
+void main(){
+  var a; // 声明一个可变的变量
+  print(a);
+
+  a = 22;
+  print(a);
+
+  a = "hello Dm";
+  print(a);
+
+  final b = 'lalal'; // 声明一个可变的变量
+  print(b);
+
+  //const 使用const声明的必须是编译
+}
